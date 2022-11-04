002f2a2d0968ad13ad6c8d64a5729ada31dc0eff
Dev-Ahmed-Emad <dr.eng.ahmed@gmail.com>
2022-11-04 22:48:22 +0200

    mod

--- request.go
+++ request.go
@@ -13,8 +13,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	waBinary "go.mau.fi/whatsmeow/binary"
-	"go.mau.fi/whatsmeow/types"
+	waBinary "github.com/AhmedEmad92/whatsmeow/binary"
+	"github.com/AhmedEmad92/whatsmeow/types"
 )
 
 func (cli *Client) generateRequestID() string {
